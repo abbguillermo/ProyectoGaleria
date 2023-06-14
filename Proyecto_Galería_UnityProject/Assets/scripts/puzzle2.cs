8a7c703e44c8c2445d984c06e3965d274a488124
JacintoPapili <papilijacinto@gmail.com>
2023-06-14 15:12:57 -0300

    cambios con los botones del segundo coso

--- Proyecto_Galería_UnityProject/Assets/scripts/puzzle2.cs
+++ Proyecto_Galería_UnityProject/Assets/scripts/puzzle2.cs
@@ -13,8 +13,13 @@ public class puzzle2 : MonoBehaviour
     public bool btn3 = false;
     public bool btn4 = false;
     public GameObject pared;
+    public GameObject interruptor1;
+    public GameObject interruptor2;
+    public GameObject interruptor3;
+    public GameObject interruptor4;
 
-   
+
+
 
     private Quaternion cuadrorot1= Quaternion.Euler(0f, 270f, 180f);
     private Quaternion cuadrorot2 = Quaternion.Euler(0f,270f, 180f);
@@ -58,18 +63,22 @@ public class puzzle2 : MonoBehaviour
     public void boton1()
     {
         btn1= true;
+        interruptor1.transform.Rotate(new Vector3(66, 0, 0));
     }
     public void boton2()
     {
         btn2 = true;
+        interruptor2.transform.Rotate(new Vector3(66, 0, 0));
     }
     public void boton3()
     {
         btn3 = true;
+        interruptor3.transform.Rotate(new Vector3(66, 0, 0));
     }
     public void boton4()
     {
         btn4 = true;
+        interruptor4.transform.Rotate(new Vector3(66, 0, 0));
     }
 
 }
