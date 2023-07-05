bc905b1b0c39f07bee783fcad160b4d93df8d4c5
loanaromero <loana.romerss@gmail.com>
2023-07-05 13:59:05 -0300

    Actualización colliders P2 y P3

--- Proyecto_Galería_UnityProject/Assets/scripts/Trigger.cs
+++ Proyecto_Galería_UnityProject/Assets/scripts/Trigger.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Trigger : MonoBehaviour
 {
     public GameObject luces;
+   /* public GameObject luces2;
+    public GameObject luces3;*/
 
     private void OnTriggerEnter(Collider other)
     {
@@ -12,6 +14,16 @@ public class Trigger : MonoBehaviour
         {
             luces.SetActive(true);
         }
+
+       /* if (other.tag == "Player" && gameObject.tag == "collider/CP2")
+        {
+            luces2.SetActive(true);
+        }
+
+        if (other.tag == "Player")
+        {
+            luces3.SetActive(true);
+        }*/
 
     }
 }
