b98dede41864fa12b970ad3e4707c224a8535199
JacintoPapili <papilijacinto@gmail.com>
2023-07-30 17:01:39 -0300

    Jachi trabajando un domingo? sisi, programacion de: posturas del enemigo, cam de muerte(quedo epico)
    para mi hay que poner mas tiempo entre teletrasnportacion, te mata muy facil

--- Proyecto_Galería_UnityProject/Assets/scripts/Sala 1/LogicaEnemigo01.cs
+++ Proyecto_Galería_UnityProject/Assets/scripts/Sala 1/LogicaEnemigo01.cs
@@ -15,6 +15,8 @@ public class LogicaEnemigo01 : MonoBehaviour
     public Vector3 pos5;
     public Vector3 pos6;
     public Vector3 pos7;
+    public GameObject camara_jugador;
+    public GameObject camara_muerte;
     // Start is called before the first frame update
     void Start()
     {
@@ -35,8 +37,11 @@ public class LogicaEnemigo01 : MonoBehaviour
 
             puedemov = false;
         }
-        
-        
+
+        if (gameObject.transform.position.x>PJ.position.x)
+        {
+            Derrota();
+        }
         
     }
 
@@ -44,14 +49,29 @@ public class LogicaEnemigo01 : MonoBehaviour
     {
         gameObject.transform.position = pos1;
         yield return new WaitForSeconds(5);
+        gameObject.transform.GetChild(1).gameObject.SetActive(false);
+        gameObject.transform.GetChild(2).gameObject.SetActive(true);
         gameObject.transform.position = pos2;
         yield return new WaitForSeconds(5);
+        gameObject.transform.GetChild(2).gameObject.SetActive(false);
+        gameObject.transform.GetChild(3).gameObject.SetActive(true);
         gameObject.transform.position = pos3;
         yield return new WaitForSeconds(5);
+        gameObject.transform.GetChild(3).gameObject.SetActive(false);
+        gameObject.transform.GetChild(4).gameObject.SetActive(true);
         gameObject.transform.position = pos4;
         yield return new WaitForSeconds(5);
+        gameObject.transform.GetChild(4).gameObject.SetActive(false);
+        gameObject.transform.GetChild(3).gameObject.SetActive(true);
         gameObject.transform.position = pos5;
         yield return new WaitForSeconds(5);
+        gameObject.transform.GetChild(3).gameObject.SetActive(false);
+        gameObject.transform.GetChild(5).gameObject.SetActive(true);
+        gameObject.transform.position = pos6;
+        yield return new WaitForSeconds(5);
+        gameObject.transform.GetChild(5).gameObject.SetActive(false);
+        gameObject.transform.GetChild(6).gameObject.SetActive(true);
+        gameObject.transform.position = pos7;
     }
     private void OnCollisionEnter(Collision collision)
     {
@@ -62,6 +82,7 @@ public class LogicaEnemigo01 : MonoBehaviour
     }
     private void Derrota()
     {
-        Debug.Log("MUERTO");
+        camara_jugador.SetActive(false);
+        camara_muerte.SetActive(true);
     }
 }
