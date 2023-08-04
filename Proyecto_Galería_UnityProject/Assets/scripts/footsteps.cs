9650307081bf074ca5a6f8fc7cbbe50e0acf4f31
loanaromero <loana.romerss@gmail.com>
2023-08-04 20:21:34 -0300

    Pasos funcionando :D

--- Proyecto_Galería_UnityProject/Assets/scripts/footsteps.cs
+++ Proyecto_Galería_UnityProject/Assets/scripts/footsteps.cs
@@ -1,11 +1,92 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class footsteps : MonoBehaviour
 {
+    public AudioClip[] steps;
+    public List<AudioClip> randomlist;
+    AudioSource source;
 
-    [SerializeField] AudioClip[] sonidos;
+    public float walkingSpeed = -1000;
+    [SerializeField]
+    AudioMixerGroup mixerOutput;
+    /*
+    [SerializeField]
+    float pitchMin = 0.95f;
+
+    [SerializeField]
+    float pitchMax = 1.85f;
+
+    [SerializeField]
+    float volumeMin = 0.95f;
+
+    [SerializeField]
+    float volumeMax = 1.00f;*/
+
+    bool playerisMoving;
+
+    void Start()
+    {
+        randomlist = new List<AudioClip>(new AudioClip[steps.Length]);
+        InvokeRepeating("CallFoosteps", 0, walkingSpeed);
+        source = gameObject.AddComponent<AudioSource>();
+        source.outputAudioMixerGroup = mixerOutput;
+
+        for (int i = 0; i < steps.Length; i++)
+        {
+            randomlist[i] = steps[i];
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetAxis("Vertical") >= 0.02f || Input.GetAxis("Horizontal") >= 0.02f || Input.GetAxis("Vertical") < 0f || Input.GetAxis("Horizontal") < 0f)
+        {
+            playerisMoving = true;
+        }
+        else if (Input.GetAxis("Vertical") == 0f || Input.GetAxis("Horizontal") == 0f) 
+        {
+            playerisMoving = false;
+        }
+    }
+
+    public void Reset()
+    {
+        for (int i = 0; i < steps.Length; i++)
+        {
+            randomlist.Add(steps[i]);
+        }
+    }
+
+    public void CallFoosteps()
+    {
+        if (playerisMoving == true)
+        {
+            PlayRandomSound();
+        }
+    }
+
+    public void PlayRandomSound()
+    {
+        int i = Random.Range(0, randomlist.Count);
+        /*source.pitch = Random.Range(pitchMin, pitchMax);
+        source.volume = Random.Range(volumeMin, volumeMax);*/
+        source.PlayOneShot(randomlist[i]);
+        randomlist.RemoveAt(i);
+
+        if (randomlist.Count == 0)
+        {
+            Reset();
+        }
+
+    }
+}
+
+
+
+    /*[SerializeField] AudioClip[] sonidos;
 
     AudioSource audiopasos;
 
@@ -31,5 +112,5 @@ public class footsteps : MonoBehaviour
         int rand = Random.Range(0, sonidos.Length);
         audiopasos.clip= sonidos[rand];
         audiopasos.Play();
-    }
-}
+    }*/
+
