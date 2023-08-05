13f1f26663bb427625655f12824a807529d9e93b
loanaromero <loana.romerss@gmail.com>
2023-08-05 20:25:38 -0300

    Importado de fuentes y funcionamiento botones básicos menú principal

---
+++ Proyecto_Galería_UnityProject/Assets/scripts/MenuPrincipal.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour
+{
+    public void Continuar()
+    {
+        //SceneManager.LoadScene(.);
+    }
+
+    public void NuevaPartida()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Logros()
+    {
+        //SceneManager.LoadScene(.);
+    }
+
+    public void Opciones()
+    {
+        //SceneManager.LoadScene(.);
+    }
+
+    public void Creditos()
+    {
+        //SceneManager.LoadScene(.);
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+        Debug.Log("Se cierra");
+    }
+}
