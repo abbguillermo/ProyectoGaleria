66a1a90b07773c1d19bfcff99ab86cbdf8d88a2c
JacintoPapili <papilijacinto@gmail.com>
2023-08-25 15:10:06 -0300

    ertjfedsojdfojdsfgopstjmfdsomdfs

--- Proyecto_Galería_UnityProject/Assets/scripts/Sala 3/puzzlesala3.cs
+++ Proyecto_Galería_UnityProject/Assets/scripts/Sala 3/puzzlesala3.cs
@@ -7,9 +7,15 @@ public class puzzlesala3 : MonoBehaviour
     public GameObject objeto1;
     public GameObject objeto2;
     public GameObject objeto3;
+    public GameObject objeto4;
+    public GameObject objeto5;
+    public GameObject objeto6;
     public bool Objeto1bool;
     public bool Objeto2bool;
     public bool Objeto3bool;
+    public bool Objeto4bool;
+    public bool Objeto5bool;
+    public bool Objeto6bool;
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +26,7 @@ public class puzzlesala3 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Objeto1bool && Objeto2bool && Objeto3bool)
+        if (Objeto1bool && Objeto2bool && Objeto3bool && Objeto4bool && Objeto5bool && Objeto6bool)
         {
             Debug.Log("bien");
         }
