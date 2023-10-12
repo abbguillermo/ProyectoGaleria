808bda4a0faed28728cf093035d29a17aad68df1
JacintoPapili <papilijacinto@gmail.com>
2023-10-12 15:20:09 -0300

    derrota sala 4

--- Proyecto_Galería_UnityProject/Assets/scripts/sala 4/Logica_Enemigo4.cs
+++ Proyecto_Galería_UnityProject/Assets/scripts/sala 4/Logica_Enemigo4.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.SceneManagement;
 
 public class Logica_Enemigo4 : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class Logica_Enemigo4 : MonoBehaviour
     public float distanciadeteccion = 10f;
     public bool sube = false;
     public bool avanza = false;
+  
 
     public bool interactuoCuadro = false;
 
@@ -17,6 +19,16 @@ public class Logica_Enemigo4 : MonoBehaviour
     public bool teleportPared2 = false;
     public bool teleportPared3 = false;
     public bool muñecoAgarrado = false;
+
+    //muerte
+    private int entro = 0;
+    public GameObject camara_jugador;
+    public GameObject camara_muerte;
+   // public GameObject enemigoCamMuerte;
+    public GameObject personaje;
+    public AudioSource audiomuerte;
+    public AudioClip grito;
+
 
     //public bool activarPalanca = false;
 
@@ -37,10 +49,21 @@ public class Logica_Enemigo4 : MonoBehaviour
 
     public float exposición_Oscuro = 7f;
 
-
+    private void Start()
+    {
+        gameObject.GetComponent<BoxCollider>().enabled = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Derrota();
+        }
+    }
     // Update is called once per frame
     void Update()
     {
+       
         if (sube&&transform.position.y<=0.2)
         {
             transform.Translate(Vector3.up * 0.3f * Time.deltaTime);
@@ -160,9 +183,36 @@ public class Logica_Enemigo4 : MonoBehaviour
             FindObjectOfType<Agarrar>().palancas4.layer = 7;
             EnemigoA1.GetComponent<Animator>().SetBool("Parte 1", true);
             EnemigoA1.GetComponent<Animator>().SetBool("Parte 2", true);
+            gameObject.GetComponent<BoxCollider>().enabled = true;
             yield return new WaitForSeconds(0.5f);
             ca.postExposure.value = 2.5f;
         }
+    }
+
+    private void Derrota()
+    {
+        camara_jugador.SetActive(false);
+        camara_muerte.SetActive(true);
+        camara_muerte.GetComponent<Animator>().SetTrigger("shake");
+        //enemigoCamMuerte.GetComponent<Animator>().Play("Muerte");
+        personaje.SetActive(false);
+
+        if (entro == 0)
+        {
+
+            audiomuerte.PlayOneShot(grito);
+
+            entro += 1;
+
+        }
+
+        StartCoroutine(pasajeescena());
+    }
+
+    IEnumerator pasajeescena()
+    {
+        yield return new WaitForSeconds(10f);
+        SceneManager.LoadScene("Sala 4");
     }
 
     void EnemigoSale()
