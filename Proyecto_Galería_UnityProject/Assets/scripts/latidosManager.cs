b6428695588943e4f46c2cf542faf4df6a66b0c8
loanaromero <loana.romerss@gmail.com>
2023-10-18 16:00:49 -0300

    Cosas en el menú, latidos y musica

--- Proyecto_Galería_UnityProject/Assets/scripts/latidosManager.cs
+++ Proyecto_Galería_UnityProject/Assets/scripts/latidosManager.cs
@@ -6,7 +6,7 @@ public class latidosManager : MonoBehaviour
 {
     public AudioClip latidoNormal;
 
-    private AudioSource latidoRapido, latidoLento;
+    private AudioSource latidoRapido /*track01*/, latidoLento;/*track02*/
     private bool reproduciendoLatidoRapido;
 
     public static latidosManager instance;
@@ -23,8 +23,10 @@ public class latidosManager : MonoBehaviour
     {
         latidoRapido = gameObject.AddComponent<AudioSource>();
         latidoRapido.loop = true;
+        //latidoRapido.volume = 1f;
         latidoLento = gameObject.AddComponent<AudioSource>();
         latidoLento.loop = true;
+        //latidoLento.volume = 1f;
         reproduciendoLatidoRapido = true;
 
         cambiarLatido(latidoNormal);
@@ -32,24 +34,47 @@ public class latidosManager : MonoBehaviour
 
     public void cambiarLatido(AudioClip newClip)
     {
-        if (reproduciendoLatidoRapido)
-        {
-            latidoLento.clip = newClip;
-            latidoLento.Play();
-            latidoRapido.Stop();
-        }
-        else
-        {
-            latidoRapido.clip = newClip;
-            latidoRapido.Play();
-            latidoLento.Stop();
-        }
-
+        StopAllCoroutines();
+        StartCoroutine(FadeTrack(newClip));
         reproduciendoLatidoRapido = !reproduciendoLatidoRapido;
     }
 
     public void volverAlNormal()
     {
         cambiarLatido(latidoNormal);
+    }
+
+    private IEnumerator FadeTrack(AudioClip newClip)
+    {
+        float timeToFade = 0.25f;
+        float timeElapsed = 0;
+
+        if (reproduciendoLatidoRapido)
+        {
+            latidoLento.clip = newClip;
+            latidoLento.Play();
+
+            while (timeElapsed < timeToFade)
+            {
+                latidoLento.volume = Mathf.Lerp(0, 1, timeElapsed / timeToFade);
+                latidoRapido.volume = Mathf.Lerp(1, 0, timeElapsed / timeToFade);
+                timeElapsed += Time.deltaTime;
+                yield return null;
+            }
+            latidoRapido.Stop();
+        }
+        else
+        {
+            latidoRapido.clip = newClip;
+            latidoRapido.Play();
+
+            while (timeElapsed < timeToFade)
+            {
+                latidoRapido.volume = Mathf.Lerp(0, 1, timeElapsed / timeToFade);
+                latidoLento.volume = Mathf.Lerp(1, 0, timeElapsed / timeToFade);
+                timeElapsed += Time.deltaTime;
+            }
+            latidoLento.Stop();
+        }
     }
 }
